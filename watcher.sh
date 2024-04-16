b099d5d36183ca4b902397a47d780efd69dd976e
Tanushree <60938591+aggarwal-tanushree@users.noreply.github.com>
2024-04-16 18:06:08 +0000

    #1 wip script

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,16 @@
-#TODO
+#!/bin/bash
+namespace="sre"
+deployment="swype-app"
+max_restarts=3
+
+while true;do
+    restarts=$(kubectl get pods -n $namespace | awk -v var="$deployment" '$0 ~ var { print $4}')
+    # echo $restarts
+    echo "Current restart count: $restarts"
+    if [[ $restarts -gt $max_restarts ]]; then 
+        echo "Restart limit exceeded. Scaling down deployment.";
+        #kubectl scale deployment $deployment -n $namespace --replicas=0
+        break 
+    fi
+    sleep 60
+done
