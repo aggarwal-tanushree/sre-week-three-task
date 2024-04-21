5e0341b881257e8e7df539eb9505eb799cfd77ba
Tanushree <60938591+aggarwal-tanushree@users.noreply.github.com>
2024-04-21 13:52:24 +0200

    #1 task-1 completion watcher.sh

--- watcher.sh
+++ watcher.sh
@@ -4,12 +4,12 @@ deployment="swype-app"
 max_restarts=3
 
 while true;do
-    restarts=$(kubectl get pods -n $namespace | awk -v var="$deployment" '$0 ~ var { print $4}')
-    # echo $restarts
+    #restarts=$(kubectl get pods -n $namespace | awk -v var="$deployment" '$0 ~ var { print $4}')
+    restarts=$(kubectl get pods -n ${namespace} -l app=${deployment} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
     echo "Current restart count: $restarts"
     if [[ $restarts -gt $max_restarts ]]; then 
         echo "Restart limit exceeded. Scaling down deployment.";
-        #kubectl scale deployment $deployment -n $namespace --replicas=0
+        kubectl scale deployment $deployment -n $namespace --replicas=0
         break 
     fi
     sleep 60
